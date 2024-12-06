40b3bdbf427882650b0dbee23787baef3fffeec8
christian <cristianyamith@hotmail.com>
2024-12-05 22:16:46 -0500

    feat: update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { AsideComponent } from './aside/aside.component';
 import { FooterComponent } from './shared/componets/footer/footer.component';
+import { HeaderComponent } from './shared/componets/header/header.component';
 
 export const routes: Routes = [
   {
@@ -21,5 +22,9 @@ export const routes: Routes = [
   {
     path: 'home',
     component: LayoutComponent,
+  },
+  {
+    path: 'header',
+    component: HeaderComponent,
   },
 ];
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AsideComponent } from '../../aside/aside.component';
 import { HeaderComponent } from '../../shared/componets/header/header.component';
 import { HomeComponent } from '../home/componets/home/home.component';
+import { FooterComponent } from '../../shared/componets/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [AsideComponent, HeaderComponent, HomeComponent],
+  imports: [AsideComponent, HeaderComponent, HomeComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
