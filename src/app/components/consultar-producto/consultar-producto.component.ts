94be3908046c10846924969849caa46091cb5215
DeivySan <deivy147@udenar.edu.co>
2024-12-09 00:01:42 -0500

    Update consultar-producto component

--- src/app/components/consultar-producto/consultar-producto.component.ts
+++ src/app/components/consultar-producto/consultar-producto.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-consultar-producto',
   standalone: true,
-  imports: [],
+  imports: [CardModule, FormsModule, ButtonModule],
   templateUrl: './consultar-producto.component.html',
   styleUrl: './consultar-producto.component.css'
 })
