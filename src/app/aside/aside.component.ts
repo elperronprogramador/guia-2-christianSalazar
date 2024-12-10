0fd692d64671c882ccf4ae48f13522b49e53b170
elperronprogramador <cristianyamith@hotmail.com>
2024-12-09 20:35:26 -0500

    feat: update home and configuration cards

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+ 
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { ListboxModule } from 'primeng/listbox';
 import { SidebarModule } from 'primeng/sidebar';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { ToolbarModule } from 'primeng/toolbar';
+import { IProduct } from '../shared/models/products.models';
+import { ProductService } from '../services/product.service';
+import { CommonModule } from '@angular/common';
 interface City {
   name: string;
   code: string;
@@ -23,6 +26,7 @@ interface City {
     SidebarModule,
     FormsModule,
     ListboxModule,
+    CommonModule
   ],
 
   templateUrl: './aside.component.html',
@@ -31,8 +35,29 @@ interface City {
 export class AsideComponent {
   @Output() onComponentSelected = new EventEmitter<string>();
 
-  onSelectComponent(componentName: string){
+  searchTerm: string = '';  
+  searchResults: IProduct[] = []; 
+
+  constructor(private productService: ProductService) {}
+
+  onSelectComponent(componentName: string) {
     this.onComponentSelected.emit(componentName);
+  }
+
+  onSearch() {
+    if (this.searchTerm.trim() !== '') {
+      this.productService.getProduct(this.searchTerm).subscribe({
+        next: (results) => {
+          this.searchResults = results;
+          console.log('Search results:', this.searchResults);
+        },
+        error: (err) => {
+          console.error('Error fetching search results:', err);
+        },
+      });
+    } else {
+      this.searchResults = [];
+    }
   }
  
 }
